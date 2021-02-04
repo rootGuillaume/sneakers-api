1cb4ecbb1fb584393e080a5e9952b436029b0303
rootGuillaume <guillaumegasmi@protonmail.com>
2021-02-04 21:00:39 +0100

    Add lxml

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 url = 'https://stockx.com/fr-fr/new-releases/sneakers'
 headers = {'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0'}
@@ -7,4 +7,6 @@ headers = {'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/
 r = requests.get(url, headers=headers)
 code = r.status_code
 
-print(r.body)
+soup = BeautifulSoup(r, 'lxml')
+
+print(soup)
